a71c21df92a9589390d45e79f28a46610f950760
prestonGarvy1 <thebozzkid2010@gmail.com>
2025-07-07 01:54:01 -0400

    Add username and password fields

--- Datapuller.py
+++ Datapuller.py
@@ -1,1 +1,20 @@
-print("it works!")
+import tkinter as tk
+import proxmoxer 
+
+# Box to enter your user and pswd
+root = tk.Tk()
+root.title("Select Node and VM")
+
+Username_label = tk.Label(root, text="Username")
+Username_label.pack()
+
+Username_entry = tk.Entry(root, width=50, )
+Username_entry.pack()
+
+pswd_label = tk.Label(root, text="Password")
+pswd_label.pack()
+
+pswd_entry = tk.Entry(root, width=50, show="*")
+pswd_entry.pack()
+
+tk.mainloop()
