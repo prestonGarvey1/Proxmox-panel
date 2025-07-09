5db38a246d74fb41ae7a3cc8e6a528244ae30812
prestonGarvy1 <thebozzkid2010@gmail.com>
2025-07-09 04:44:34 -0400

    remove old tkinter and replace with a more modern gui

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tkinter  as tk
-from tkinter import ttk 
+import customtkinter
 import webview as web
 import os
 
@@ -15,36 +14,36 @@ def load_dataview():
     
     
 
-root = tk.Tk()
+root = customtkinter.CTk()
 root.title("Enter your URL")
-
+root.geometry("400x400")
 #Inputs for web url and port
-url_label = tk.Label(root, text="URL or IP:")
+url_label = customtkinter.CTkLabel(root, text="URL or IP:")
 url_label.pack()
 
-url_entry = tk.Entry(root, width=50)
+url_entry = customtkinter.CTkEntry(root, width=100)
 url_entry.pack()
 
-port_label = tk.Label(root, text="(Optional) Port:")
+port_label = customtkinter.CTkLabel(root, text="(Optional) Port:")
 port_label.pack()
 
-port_entry = tk.Entry(root, width=50) #if a port is needed, this option supplies it
-port_entry.insert(tk.END, "80")
+port_entry = customtkinter.CTkEntry(root, width=100) #if a port is needed, this option supplies it
+port_entry.insert(customtkinter.END, "80")
 port_entry.pack()
 
-dir_label = tk.Label(root, text="(Optional) Directory:")
+dir_label = customtkinter.CTkLabel(root, text="(Optional) Directory:")
 dir_label.pack()
 
-dir_entry = tk.Entry(root, width=50)
+dir_entry = customtkinter.CTkEntry(root, width=100)
 dir_entry.pack()
 
 
 # Proxmox data viewer button
-data_button = tk.Button(root, text="Data View", command=load_dataview)
-data_button.pack()
+data_button = customtkinter.CTkButton(root, text="Data View", command=load_dataview)
+data_button.pack(padx=10, pady=10)
 # Load webview
-load_button = tk.Button(root, text="Load Page", command=load_page)
-load_button.pack()
+load_button = customtkinter.CTkButton(root, text="Load Page", command=load_page)
+load_button.pack(padx=10, pady=10)
 
 root.mainloop() 
 
