04a49d582f53cbaad394008c8011ac9bfaa09aec
Minorridge <ridgetolliver3@gmail.com>
2025-08-15 09:02:05 -0400

    Update Datapuller.py to use customtkinter

--- Datapuller.py
+++ Datapuller.py
@@ -1,4 +1,4 @@
-import tkinter as tk
+import customtkinter
 import tkinter.messagebox as messagebox
 import proxmoxer 
 from proxmoxer import ProxmoxAPI
@@ -10,33 +10,34 @@ import sys
 
 
 # Box to enter your user and pswd
-root = tk.Tk()
+root = customtkinter.CTk()
 root.title("Select Node and VM")
+root.geometry("400x400")
 
-host_label = tk.Label(root, text="Host:")
+host_label = customtkinter.CTkLabel(root, text="Host:")
 host_label.pack()
 
-host_entry = tk.Entry(width=50)
-host_entry.pack()
+host_Entry = customtkinter.CTkEntry(root, width=50)
+host_Entry.pack()
 
-Username_label = tk.Label(root, text="Username:")
+Username_label = customtkinter.CTkLabel(root, text="Username:")
 Username_label.pack()
 
-Username_entry = tk.Entry(root, width=50 )
-Username_entry.pack()
+Username_Entry = customtkinter.CTkEntry(root, width=50 )
+Username_Entry.pack()
 
 
-pass_label = tk.Label(root, text="Password:")
+pass_label = customtkinter.CTkLabel(root, text="Password:")
 pass_label.pack()
 
-pass_entry = tk.Entry(root, width=50, show="*")
-pass_entry.pack()
+pass_CTkEntry = customtkinter.CTkEntry(root, width=50, show="*")
+pass_CTkEntry.pack()
 
 
 def ProxAPI():
-    Hostname = host_entry.get()
-    User = Username_entry.get()
-    Password = pass_entry.get()
+    Hostname = host_Entry.get()
+    User = Username_Entry.get()
+    Password = pass_CTkEntry.get()
     try:
         API = ProxmoxAPI(f'{Hostname}', user=f'{User}', password=f'{Password}', backend='ssh_paramiko', service='PVE')
         
@@ -45,9 +46,9 @@ def ProxAPI():
    
 
 def getusers():
-    Hostname = host_entry.get()
-    User = Username_entry.get()
-    Password = pass_entry.get()
+    Hostname = host_Entry.get()
+    User = Username_Entry.get()
+    Password = pass_CTkEntry.get()
     API = ProxmoxAPI(f'{Hostname}', user=f'{User}', password=f'{Password}', backend='ssh_paramiko', service='PVE')
         
     with open('users.json', 'w',) as f:
@@ -57,11 +58,11 @@ def getusers():
     
 
 
-apiauth_button = tk.Button(root, text="Check Authentication", command=ProxAPI)
-apiauth_button.pack()
+apiauth_button = customtkinter.CTkButton(root, text="Check Authentication", command=ProxAPI)
+apiauth_button.pack(padx=10, pady=10)
 
-getusers_button = tk.Button(root, text='Dump users file', command=getusers)
-getusers_button.pack()
-tk.mainloop()
+getusers_button = customtkinter.CTkButton(root, text='Dump users file', command=getusers)
+getusers_button.pack(padx=10, pady=10)
+root.mainloop()
 
 
